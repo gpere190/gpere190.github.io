07c468983efe683a0ee76ee5814f249aa71ccfb6
Gricelle Perez <41925770+gpere190@users.noreply.github.com>
2025-04-18 17:25:34 -0400

    Update profile.js

--- profile.js
+++ profile.js
@@ -2,16 +2,16 @@
   const openModalBtn = document.getElementById("openModalBtn");
   const closeModalBtn = document.getElementById("closeModalBtn");
 
-  openModalBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+  openModalBtn.addEventListener("click", function(e) {
+    e.preventDefault(); // prevent page jump
     modal.style.display = "block";
   });
 
-  closeModalBtn.addEventListener("click", () => {
+  closeModalBtn.addEventListener("click", function() {
     modal.style.display = "none";
   });
 
-  window.addEventListener("click", (e) => {
+  window.addEventListener("click", function(e) {
     if (e.target === modal) {
       modal.style.display = "none";
     }
